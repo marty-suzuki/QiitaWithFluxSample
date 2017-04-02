2529feed7758ba963a369261815368e2a7502741
marty-suzuki <s1180183@gmail.com>
2017-04-02 23:23:34 +0900

    add Project

---
+++ QiitaWithFluxSample/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RootViewController.swift
+//  QiitaWithFluxSample
+//
+//  Created by marty-suzuki on 2017/04/02.
+//  Copyright © 2017年 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
