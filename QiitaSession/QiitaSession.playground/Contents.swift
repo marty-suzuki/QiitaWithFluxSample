c2ebd4643c3b50e345337bb7df9ac95b7bffe6df
marty-suzuki <s1180183@gmail.com>
2017-06-14 02:53:14 +0900

    fix playground

--- QiitaSession/QiitaSession.playground/Contents.swift
+++ QiitaSession/QiitaSession.playground/Contents.swift
@@ -1,5 +1,26 @@
-//: Playground - noun: a place where people can play
-
+/*:
+ > # IMPORTANT: To use **QiitaSession.playground**:
+ 1. Open **QiitaWithFluxSample.xcodeproj**.
+ 1. Build the **QiitaSession** scheme (**Product** → **Build**).
+ 1. Open **QiitaSession** playground in the **Project navigator**.
+ 1. Show the Debug Area (**View** → **Debug Area** → **Show Debug Area**).
+ ---
+ */
+import PlaygroundSupport
 import QiitaSession
 
-let session = QiitaSession.shared
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+QiitaRequestConfig.baseUrl = "https://qiita.com/api"
+let request = ItemsRequest(page: 1, perPage: 3, query: "marty-suzuki")
+_ = QiitaSession.shared.send(request)
+    .subscribe(
+        onNext: { response in
+            print(response.elements)
+            PlaygroundPage.current.finishExecution()
+        },
+        onError: { error in
+            print(error)
+            PlaygroundPage.current.finishExecution()
+        }
+    )
